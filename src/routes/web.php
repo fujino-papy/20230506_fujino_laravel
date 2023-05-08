12c470b8e0b4a4ef76c0370265db4cec57160408
fujino <papy41537@gmail.com>
2023-05-08 21:47:20 +0900

    任意のメッセージ

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,4 +6,4 @@ use App\Http\Controllers\AuthorController;
 Route::get('/find', [AuthorController::class, 'find']);
 Route::post('/find', [AuthorController::class, 'search']);
 Route::get('/author/{author}', [AuthorController::class, 'bind']);
-Route::get('index', [AuthorController::class, 'index']);
+Route::get('/', [AuthorController::class, 'index']);
